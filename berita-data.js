32e940ad309fe5a5e2cb4a64d81f4d8b47b7a58a
WEBSIPAI <wildantanjung4@gmail.com>
2025-09-28 17:14:11 +0700

    Add files via upload

---
+++ berita-data.js
@@ -0,0 +1,25 @@
+// Data berita terbaru Prodi PAI
+// Tambahkan berita baru di array ini
+const beritaList = [
+  {
+    tanggal: "16 Juli 2025",
+    judul: "Kegiatan Visitasi Akreditasi Prodi PAI Oleh BAN-PT",
+    gambar: "D:\\UINFAS\\WEBSIPAI\\Berita\\asset\\Akreditasi PAI.png",
+    deskripsi: "Prodi Pendidikan Agama Islam UIN Fatmawati Sukarno Bengkulu berhasil meraih akreditasi A dari BAN-PT. Capaian ini menjadi bukti komitmen prodi dalam menjaga mutu pendidikan dan pelayanan kepada mahasiswa.",
+    link: "https://ftt.uinfasbengkulu.ac.id/prodi-pendidikan-agama-islam-uin-fatmawati-sukarno-bengkulu-jalani-asesmen-lapangan-akreditasi/"
+  },
+  {
+    tanggal: "20 September 2025",
+    judul: "Seminar Nasional Pendidikan Islam",
+    gambar: "D:\\UINFAS\\WEBSIPAI\\Berita\\asset\\Seminar Nasional Pendidikan Islam.png",
+    deskripsi: "Seminar nasional menghadirkan narasumber dari berbagai universitas ternama membahas inovasi pembelajaran PAI di era digital.",
+    link: "#"
+  },
+  {
+    tanggal: "10 September 2025",
+    judul: "Pengabdian Masyarakat Mahasiswa PAI",
+    gambar: "https://via.placeholder.com/400x200?text=Berita+3",
+    deskripsi: "Mahasiswa PAI melaksanakan pengabdian masyarakat di desa binaan, mengajar dan berbagi inspirasi kepada anak-anak sekolah dasar.",
+    link: "#"
+  }
+];
