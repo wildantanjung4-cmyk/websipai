464a9f1400d11054e45ffacf8447574e6e9a7cdb
WEBSIPAI <wildantanjung4@gmail.com>
2025-09-29 16:59:17 +0700

    Update berita-data.js

--- berita-data.js
+++ berita-data.js
@@ -13,16 +13,17 @@ const beritaList = [
     judul: "Kuliah Tamu Prodi Pendidikan Agama Islam",
     gambar: "https://irw51owxi5vo3sjd.public.blob.vercel-storage.com/asset-berita/Screenshot%202025-09-18%20142336.png",
     deskripsi: "Program Studi Pendidikan Agama Islam sukses menyelenggarakan kuliah tamu dengan tema “Revitalisasi Profesi Guru: Menjawab Tantangan dan Mengubah Paradigma Negatif”.",
-    link: "#"
+    link: "https://ftt.uinfasbengkulu.ac.id/prodi-pendidikan-agama-islam-uin-fatmawati-sukarno-bengkulu-jalani-asesmen-lapangan-akreditasi/"
   },
   {
     tanggal: "10 September 2025",
     judul: "Pengabdian Masyarakat Mahasiswa PAI",
     gambar: "https://via.placeholder.com/400x200?text=Berita+3",
     deskripsi: "Mahasiswa PAI melaksanakan pengabdian masyarakat di desa binaan, mengajar dan berbagi inspirasi kepada anak-anak sekolah dasar.",
-    link: "#"
+    link: "https://ftt.uinfasbengkulu.ac.id/prodi-pendidikan-agama-islam-uin-fatmawati-sukarno-bengkulu-jalani-asesmen-lapangan-akreditasi/"
   }
 ];
+
 
 
 
