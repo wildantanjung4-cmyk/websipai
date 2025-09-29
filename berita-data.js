6c89c8d4da9820e2732d25e8876267907e1d9cc3
WEBSIPAI <wildantanjung4@gmail.com>
2025-09-29 16:29:22 +0700

    Update berita-data.js

--- berita-data.js
+++ berita-data.js
@@ -4,14 +4,14 @@ const beritaList = [
   {
     tanggal: "16 Juli 2025",
     judul: "Kegiatan Visitasi Akreditasi Prodi PAI Oleh BAN-PT",
-    gambar: "/workspaces/websipai/Berita/asset/Akreditasi PAI.png",
+    gambar: "https://irw51owxi5vo3sjd.public.blob.vercel-storage.com/asset-berita/Akreditasi%20PAI.png",
     deskripsi: "Prodi Pendidikan Agama Islam UIN Fatmawati Sukarno Bengkulu berhasil meraih akreditasi A dari BAN-PT. Capaian ini menjadi bukti komitmen prodi dalam menjaga mutu pendidikan dan pelayanan kepada mahasiswa.",
     link: "https://ftt.uinfasbengkulu.ac.id/prodi-pendidikan-agama-islam-uin-fatmawati-sukarno-bengkulu-jalani-asesmen-lapangan-akreditasi/"
   },
   {
     tanggal: "20 September 2025",
-    judul: "Seminar Nasional Pendidikan Islam",
-    gambar: "D:\\UINFAS\\WEBSIPAI\\Berita\\asset\\Seminar Nasional Pendidikan Islam.png",
+    judul: "Kuliah Tamu Prodi Pendidikan Agama Islam",
+    gambar: "https://irw51owxi5vo3sjd.public.blob.vercel-storage.com/asset-berita/Screenshot%202025-09-18%20142336.png",
     deskripsi: "Seminar nasional menghadirkan narasumber dari berbagai universitas ternama membahas inovasi pembelajaran PAI di era digital.",
     link: "#"
   },
@@ -23,3 +23,4 @@ const beritaList = [
     link: "#"
   }
 ];
+
