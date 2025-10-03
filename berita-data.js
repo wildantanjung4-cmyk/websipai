92d42e72a5d92c21c1a55e0068850636e9b6f8e1
WEBSIPAI <wildantanjung4@gmail.com>
2025-10-03 14:43:54 +0700

    Update berita-data.js

--- berita-data.js
+++ berita-data.js
@@ -19,10 +19,11 @@ const beritaList = [
     tanggal: "02 Oktober 2025",
     judul: "Pembekalan Program Pesantrenisasi Mahasiswa PAI UIN Fatmawati Sukarno Bengkulu",
     gambar: "https://irw51owxi5vo3sjd.public.blob.vercel-storage.com/asset-berita/Pesantrenisasi%20PAI%20UINFAS/WhatsApp%20Image%202025-10-02%20at%2015.40.07.jpeg",
-    deskripsi: "Program Studi Pendidikan Agama Islam (PAI) UIN Fatmawati Sukarno Bengkulu kembali menunjukkan komitmennya dalam penguatan karakter dan nilai-nilai keislaman melalui kegiatan Pembekalan Program Pesantrenisasi bagi mahasiswa baru. Kegiatan ini dirancang untuk membekali mahasiswa dengan pemahaman dasar mengenai kehidupan pesantren, nilai-nilai keislaman, serta etika keilmuan yang akan menjadi fondasi selama menempuh pendidikan di lingkungan kampus berbasis keislaman.",
+    deskripsi: "Program Studi Pendidikan Agama Islam (PAI) UIN Fatmawati Sukarno Bengkulu mengadakan kegiatan Pembekalan Program Pesantrenisasi bagi mahasiswa baru sebagai upaya menanamkan nilai-nilai keislaman sejak dini.",
     link: "https://ftt.uinfasbengkulu.ac.id/prodi-pendidikan-agama-islam-uin-fatmawati-sukarno-bengkulu-jalani-asesmen-lapangan-akreditasi/"
   }
 ];
+
 
 
 
